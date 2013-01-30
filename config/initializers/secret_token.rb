57091a21cc99c382eb719567cc57e3647be0434b
Martin Xu <Martin.Xu@move.com>
2013-01-30 09:23:21 -0800

    added initial rails code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentalsAPIDeploymentVerification::Application.config.secret_token = '2a52d1e851a96d840557ad1f6a37554c5a80a55138273c14f837449830442447925222bde6c50a7251d0580a819ac95de20e3e67f61e262baf930fd2a094918d'
