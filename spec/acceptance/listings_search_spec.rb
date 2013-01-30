ae6f51f8b7b530f6e966ca33e5669d3580ebf20d
Martin Xu <Martin.Xu@move.com>
2013-01-30 13:29:14 -0800

    added two more tests

--- spec/acceptance/listings_search_spec.rb
+++ spec/acceptance/listings_search_spec.rb
@@ -23,4 +23,24 @@ describe '/listings/search' do
   end
 
 
+  it "Return 20 results instead of default 10 starting at page 3 (offset=[0.. 1.. 2]) "  do
+    url =  SERVICE_URL+common_part+"&limit=20&offset=2"
+    resp = HTTParty.get(url)
+    resp["returned_rows"].should == 20
+  end
+
+  it "Request Community Listings which are Showcase and allow cats"  do
+    url =  SERVICE_URL+common_part+"&cats=true&type=community&showcase=true"
+    resp = HTTParty.get(url)
+    resp["returned_rows"].should >0
+  end
+
+  it "Request all rental listings on McDowell Rd in Avondale, AZ"  do
+    url =  SERVICE_URL+common_part+"&address=\"McDowell Rd\"&city=\"Avondale\"&state_code=\"AZ\""
+    resp = HTTParty.get(url)
+    resp["returned_rows"].should >0
+  end
+
+
+
 end
