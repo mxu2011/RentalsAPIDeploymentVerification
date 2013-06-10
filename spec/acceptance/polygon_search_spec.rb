dd6865a02bc1921866ff8fbd6e267c24cc779cfa
Martin Xu <Martin.Xu@move.com>
2013-06-10 01:53:11 -0700

    fixed a minor bug for test url assebmly

--- spec/acceptance/polygon_search_spec.rb
+++ spec/acceptance/polygon_search_spec.rb
@@ -3,12 +3,10 @@ require 'spec_helper'
 describe ' polygonsearch' do
 
   it "Should not return 500 error "  do
-    url =  SERVICE_URL
 
     File.open("#{Rails.root}/spec/fixtures/polygon_prod_errors.json").each_line do |line|
       puts line
-      url+=JSON.parse(line)["result"]["http_request"].scan( /GET \/v1\/.+points.+HTTP/)[0].gsub("GET \/v1",'').gsub("HTTP",'')
-
+      url= SERVICE_URL + JSON.parse(line)["result"]["http_request"].scan( /GET \/v1\/.+points.+HTTP/)[0].gsub("GET \/v1",'').gsub("HTTP",'')
       resp = HTTParty.get(url)
       resp.should_not nil
     end
