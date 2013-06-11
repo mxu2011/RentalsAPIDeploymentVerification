f8251f6a3ba1445ea874806f58b9e73893661433
Martin Xu <Martin.Xu@move.com>
2013-06-11 14:41:16 -0700

    added polygon_prod_all.json

--- spec/acceptance/polygon_search_spec.rb
+++ spec/acceptance/polygon_search_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ' polygonsearch' do
 
-  it "Should not return 500 error "  do
+  it "production polygon search having 500 error "  do
 
     count = 0
     err_count = 0
@@ -17,6 +17,31 @@ describe ' polygonsearch' do
         end
 
         rescue
+        puts "Exception ERROR----##{count}:#{line}\n"
+        err_count += 1
+      end
+      count +=1
+      puts "#{err_count} out of #{count} failed"
+
+    end
+
+  end
+
+  it "All production polygon searches"  do
+
+    count = 0
+    err_count = 0
+    File.open("#{Rails.root}/spec/fixtures/polygon_prod_all.json").each_line do |line|
+      begin
+        url= SERVICE_URL + JSON.parse(line)["result"]["http_request"].scan( /GET \/v1\/.+points.+HTTP/)[0].gsub("GET \/v1",'').gsub("HTTP",'')
+        resp = HTTParty.get(url)
+
+        if !resp.instance_of?(Hash)
+          puts "Nice ERROR----##{count}:#{line}\n"
+          err_count += 1
+        end
+
+      rescue
         puts "Exception ERROR----##{count}:#{line}\n"
         err_count += 1
       end
